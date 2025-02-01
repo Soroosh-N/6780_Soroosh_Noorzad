a0a7a9aeebae0b9a7d304b72cde2d8cfac59e985
Soroosh Noorzad - U1477303 <soroosh.noorzad@utah.edu>
2025-01-31 17:11:49 -0700

    Made Assertion for GPIO C and A

--- Src/lab1.c
+++ Src/lab1.c
@@ -9,10 +9,18 @@ int lab1_main(void) {
     with hardware register access. */
     __HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC
     // Set up a configuration struct to pass to the initialization function
-    GPIO_InitTypeDef initStr = {GPIO_PIN_8 | GPIO_PIN_9, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
-    HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC8 & PC9
+    // GPIO-C
+    GPIO_InitTypeDef initStrGPC = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
+    HAL_GPIO_Init(GPIOC, &initStrGPC); // Initialize pins PC8 & PC9
+    HAL_Delay(1);  // Optional delay to ensure the initialization is complete
+    assert(GPIOC->MODER == 0x55000); // Assertion on GPIOC
+    // GPIO-A
+    GPIO_InitTypeDef initStrGPA = {GPIO_PIN_0, GPIO_MODE_INPUT, GPIO_SPEED_FREQ_LOW, GPIO_PULLDOWN};
+    HAL_GPIO_Init(GPIOA, &initStrGPA); // Initialize pins PA0
+    HAL_Delay(1);  // Optional delay to ensure the initialization is complete
+    // assert(GPIOA->MODER == 0x0); // Assertion on GPIOA
+    assert((GPIOA->MODER & 0x03) == 0x00);  // Assert that PA0 is configured as input (bits 0 and 1 should be 00)
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // Start PC8 high
-    assert(GPIOC->MODER == 0x50000);
     while (1) {
         HAL_Delay(200); // Delay 200ms
         // Toggle the output state of both PC8 and PC9

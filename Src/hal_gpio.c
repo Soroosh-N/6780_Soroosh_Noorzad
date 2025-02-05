d2631785ec52198b3aa11504d94d880d5fe26724
Soroosh Noorzad <soroosh.noorzad@utah.edu>
2025-02-05 00:51:47 -0700

    Implemented hal_gpio.c file to replace with the HAL_GPIO default functions.

--- Src/hal_gpio.c
+++ Src/hal_gpio.c
@@ -1,11 +1,41 @@
 #include <stdint.h>
 #include <stm32f0xx_hal.h>
 #include <stm32f0xx_hal_gpio.h>
-/*
-void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
-{
+
+void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init){
+    if (GPIOx == GPIOA) {
+        // Configure PA0 as Digital Input
+        // Set as input mode
+        GPIOA->MODER &= ~(0x3);
+        // Configure PA0 speed as Low Speed
+        GPIOA->OSPEEDR &= ~(0x3);
+        // Configure PA0 with Pull-Down resistor
+        GPIOA->PUPDR &= ~(0x3);
+        GPIOA->PUPDR |= (0x2);
+    } else if (GPIOx == GPIOC) {
+        // Configure PC6, PC7, PC8, PC9 as General Purpose Output
+        // Clear bits
+        GPIOC->MODER &= ~(0xFF000);
+        // Set as output mode
+        GPIOC->MODER |= 0x55000;
+        // Configure output type as Push-Pull
+        GPIOC->OTYPER &= ~(0xF<<6);
+        // Configure output speed as Low Speed
+        GPIOC->OSPEEDR &= ~(0xFF000);
+        // No pull-up or pull-down resistors
+        GPIOC->PUPDR &= ~(0xFF000);
+    } else {
+        // Two long red flash (Error):
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+        HAL_Delay(2000);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
+        HAL_Delay(100);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
+        HAL_Delay(2000);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
+    }
 }
-*/
+
 
 /*
 void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
--- Src/lab1.c
+++ Src/lab1.c
@@ -1,4 +1,5 @@
 #include <stm32f0xx_hal.h>
+#include <hal_gpio.h>
 #include <assert.h>
 #include <main.h>
 
@@ -11,32 +12,50 @@ int lab1_main(void) {
     // Enable the GPIOC clock in the RCC
     __HAL_RCC_GPIOC_CLK_ENABLE();
     // Set up a configuration struct to pass to the initialization function
+
+    // // ************ Check off 1 ************
+    // GPIO_InitTypeDef initStrGPC = {0};
+    // // GPIO-C:
+    // initStrGPC.Pin = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
+    // // GPIOx_OTYPER – Push-Pull Output Type
+    // initStrGPC.Mode = GPIO_MODE_OUTPUT_PP;
+    // // GPIOx_PUPDR – No Pull-Up or Pull-Down
+    // initStrGPC.Pull = GPIO_NOPULL;
+    // // GPIOx_OSPEEDR – Low Speed
+    // initStrGPC.Speed = GPIO_SPEED_FREQ_LOW;
+    // // Initialize pins
+    // HAL_GPIO_Init(GPIOC, &initStrGPC);
+    // // Optional delay to ensure the initialization is complete
+    // HAL_Delay(1);
+    // // Assertion on GPIOC (All the pins should have the 01 value => 01010101 = 55)
+    // assert(GPIOC->MODER == 0x55000);
+    // // GPIO-A: Configure PA0 as Digital Input (bits 0 and 1 should be 00)
+    // initStrGPC.Pin = GPIO_PIN_0;
+    // // USER Button (input):
+    // initStrGPC.Mode = GPIO_MODE_INPUT;
+    // // GPIOx_PUPDR – Pull-Down Resistor
+    // initStrGPC.Pull = GPIO_PULLDOWN;
+    // initStrGPC.Speed = GPIO_SPEED_FREQ_LOW;
+    // HAL_GPIO_Init(GPIOA, &initStrGPC);
+    // HAL_Delay(1);
+    // // Assert that PA0 is configured as input (00)
+    // assert((GPIOA->MODER & 0x03) == 0x00);
+
+    // ************ Check off 2 ************
     GPIO_InitTypeDef initStrGPC = {0};
     // GPIO-C:
-    initStrGPC.Pin = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
-    // GPIOx_OTYPER – Push-Pull Output Type
-    initStrGPC.Mode = GPIO_MODE_OUTPUT_PP;
-    // GPIOx_PUPDR – No Pull-Up or Pull-Down
-    initStrGPC.Pull = GPIO_NOPULL;
-    // GPIOx_OSPEEDR – Low Speed
-    initStrGPC.Speed = GPIO_SPEED_FREQ_LOW;
     // Initialize pins
-    HAL_GPIO_Init(GPIOC, &initStrGPC);
+    My_HAL_GPIO_Init(GPIOC, &initStrGPC);
     // Optional delay to ensure the initialization is complete
     HAL_Delay(1);
     // Assertion on GPIOC (All the pins should have the 01 value => 01010101 = 55)
     assert(GPIOC->MODER == 0x55000);
     // GPIO-A: Configure PA0 as Digital Input (bits 0 and 1 should be 00)
-    initStrGPC.Pin = GPIO_PIN_0;
-    // USER Button (input):
-    initStrGPC.Mode = GPIO_MODE_INPUT;
-    // GPIOx_PUPDR – Pull-Down Resistor
-    initStrGPC.Pull = GPIO_PULLDOWN;
-    initStrGPC.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOA, &initStrGPC);
+    My_HAL_GPIO_Init(GPIOA, &initStrGPC);
     HAL_Delay(1);
     // Assert that PA0 is configured as input (00)
     assert((GPIOA->MODER & 0x03) == 0x00);
+    
     // Start PC8 high
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
     while (1) {

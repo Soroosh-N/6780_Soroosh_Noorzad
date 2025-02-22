3699591e3c699c53e3298935bf06971ce57f1b71
Soroosh Noorzad <soroosh.noorzad@utah.edu>
2025-02-22 04:06:44 -0700

    Lab 4: Check off 2 completed, Interrupted BTN and 2character input parser has been implemented.

--- Src/lab4.c
+++ Src/lab4.c
@@ -11,15 +11,39 @@ PB11,   PB10,   PB14,   PB13,   PB12,
 PD9,    PD8,    PD12,   PD11,   PD10,
 PC11,   PC10,   PD2,    -   ,   PC12,
 */
-//  PB10 and PB11
+// Recommended by TA: PB10 and PB11
+
+// Global variables for interrupt handling
+volatile char received_data = 0;
+volatile uint8_t data_available = 0;
+
 void USART_Init(void) {
     // USART3
     My_HAL_RCC_USART_CLK_ENABLE();              // Enable clock
     uint32_t pclk = HAL_RCC_GetHCLKFreq();      // System clock frequency (Typically 48MHz for STM32F0)
     USART3->BRR = pclk / 115200;                // Set baud rate to 115200
     USART3->CR1 |= USART_CR1_TE | USART_CR1_RE; // Enable Transmitter and Receiver
-    // Enable USART3
-    USART3->CR1 |= USART_CR1_UE;
+    USART3->CR1 |= USART_CR1_RXNEIE;            // Enable RXNE (Receive Register Not Empty) interrupt
+    USART3->CR1 |= USART_CR1_UE;                // Enable USART3
+    NVIC_EnableIRQ(USART3_4_IRQn);              // Enable USART3 interrupt in NVIC
+    NVIC_SetPriority(USART3_4_IRQn, 2);         // Set priority
+}
+
+// Interrupt handler for USART3
+void USART3_4_IRQHandler(void) {
+    received_data = USART3->RDR;    // Read received character (automatically clears RXNE flag)
+    data_available = 1;             // Set flag indicating new data
+}
+
+// Non-blocking function to check if data is available
+uint8_t USART3_IsDataAvailable(void) {
+    return data_available;
+}
+
+// Function to get received character (clears flag)
+char USART3_GetChar(void) {
+    data_available = 0; // Clear flag
+    return received_data;
 }
 
 void USART3_TransmitChar(char c) {
@@ -28,15 +52,10 @@ void USART3_TransmitChar(char c) {
 }
 
 void USART3_TransmitString(const char *str) {
-    while (*str) {               // Loop until null character ('\0') is encountered
-        USART3_TransmitChar(*str); // Transmit
+    while (*str) {                  // Loop until null character ('\0') is encountered
+        USART3_TransmitChar(*str);  // Transmit
         str++;
     }
-}
-
-char USART3_ReceiveChar(void) {
-    while (!(USART3->ISR & USART_ISR_RXNE)); // Wait for RXNE (data received)
-    return USART3->RDR; // Read received character
 }
 
 void PA0_Interrupt_Init(void) {
@@ -49,12 +68,11 @@ void PA0_Interrupt_Init(void) {
     EXTI->IMR |= EXTI_IMR_IM0;                      // Unmask interrupt line 0
     NVIC_EnableIRQ(EXTI0_1_IRQn);                   // Enable EXTI0 interrupt in NVIC
     NVIC_SetPriority(EXTI0_1_IRQn, 3);              // Set priority
-    NVIC_SetPriority(SysTick_IRQn, 0);              // Set Systick priority
 }
 
 void EXTI0_1_IRQHandler(void) {
-    USART3_TransmitString("Button Pressed!\r\n");
-    GPIOC->ODR &= ~(GPIO_PIN_6);
+    USART3_TransmitString("Button Pressed! Red LED will toggle 3 times.\r\n\r\nCMD?\r\n");
+    My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
     for (uint32_t i = 0; i < 6; i++) {
         My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
         volatile uint32_t C = 0;
@@ -65,9 +83,56 @@ void EXTI0_1_IRQHandler(void) {
     EXTI->PR |= EXTI_PR_PR0; // Clear interrupt flag (write 1 to clear)
 }
 
+// Function to control LEDs based on command
+void Process_Command(char color, char action) {
+    uint16_t led_pin = 0;
+    
+    // Identify LED color
+    switch (color) {
+        case 'r': led_pin = GPIO_PIN_6; break; // Red LED
+        case 'g': led_pin = GPIO_PIN_9; break; // Green LED
+        case 'o': led_pin = GPIO_PIN_8; break; // Orange LED
+        default:
+            USART3_TransmitChar(color); // Transmit received char
+            USART3_TransmitString(" is not a valid LED **COLOR** command!\r\n");
+            My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_RESET);
+            for (uint32_t i = 0; i < 6; i++) {
+                My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_8 | GPIO_PIN_9);
+                HAL_Delay(300);
+            }
+            return;
+    }
+
+    // Perform action
+    switch (action) {
+        case '0': // Turn OFF
+            My_HAL_GPIO_WritePin(GPIOC, led_pin, GPIO_PIN_RESET);
+            USART3_TransmitString("Turned OFF\r\n");
+            break;
+        case '1': // Turn ON
+            My_HAL_GPIO_WritePin(GPIOC, led_pin, GPIO_PIN_SET);
+            USART3_TransmitString("Turned ON\r\n");
+            break;
+        case '2': // Toggle
+            My_HAL_GPIO_TogglePin(GPIOC, led_pin);
+            USART3_TransmitString("TOGGLED\r\n");
+            break;
+        default:
+            USART3_TransmitChar(action); // Transmit received char
+            USART3_TransmitString(" is not a valid LED **ACTION** command!\r\n");
+            My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_RESET);
+            for (uint32_t i = 0; i < 6; i++) {
+                My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_8 | GPIO_PIN_9);
+                HAL_Delay(300);
+            }
+            return;
+    }
+}
+
 int lab4_main(void) {
     // Configure the system clock
     SystemClock_Config();
+    NVIC_SetPriority(SysTick_IRQn, 0);  // Set Systick priority
     GPIO_InitTypeDef initStr = {0};
 
     // GPIO-A
@@ -83,36 +148,18 @@ int lab4_main(void) {
     GPIOC->MODER |= (0xA << (4 * 2));   // Set PC4 and PC5 to AF mode (10)
     // Select AF1 (USART3)
     GPIOC->AFR[0] &= ~(0xFF << 4 * 4);  // Clear AF bits for PC4 (4 bits) and PC5 (4 bits)
-    GPIOC->AFR[0] |= (0x11 << 4 * 4); // Set AF1 for PC4 and Pc5 in AF Register
+    GPIOC->AFR[0] |= (0x11 << 4 * 4);   // Set AF1 for PC4 and Pc5 in AF Register
 
     USART_Init();
     while (1) {
-        // Receiving Character
-        char received = USART3_ReceiveChar(); // Get character from terminal
-        switch (received) {
-            case 'r':
-                USART3_TransmitString("R => Red LED Toggled!\r\n");
-                My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);   // Toggle Red LED (PC6)
-                break;
-            case 'g':
-                USART3_TransmitString("G => Green LED Toggled!\r\n");
-                My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);   // Toggle Green LED (PC9)
-                break;
-            case 'o':
-                USART3_TransmitString("O => Orange LED Toggled!\r\n");
-                My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);   // Toggle Blue LED (PC7)
-                break;
-            default:
-                USART3_TransmitChar(received); // Transmit received char
-                USART3_TransmitString(" is not a command!\r\n");
-                GPIOC->ODR &= ~(GPIO_PIN_6 | GPIO_PIN_8 | GPIO_PIN_9);
-                for (uint32_t i = 0; i < 6; i++) {
-                    My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
-                    My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
-                    My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
-                    HAL_Delay(300);
-                }
-                break;
-        }
+        USART3_TransmitString("\r\nCMD?\r\n");  // Print command prompt
+        while (!USART3_IsDataAvailable());      // Wait for first character (LED color)
+        char color = USART3_GetChar();          // Get 1st Char
+        USART3_TransmitChar(color);             // Display the input as a feedback
+        while (!USART3_IsDataAvailable());      // Wait for second character (Action)
+        char action = USART3_GetChar();         // Get 2nd Char
+        USART3_TransmitChar(action);            // Display the input as a feedback
+        USART3_TransmitString(" => ");
+        Process_Command(color, action);         // Process command
     }
 }
